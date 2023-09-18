2e831729b879efa3e5639458789d493ef9049466
IjjazAhmad <ijjazahmad705@gmail.com>
2023-09-18 14:56:39 +0500

    modify

--- src/Pages/Auth/Login.js
+++ src/Pages/Auth/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useAuthContext } from '../../Context/AuthContext'
 import { message } from 'antd';
-const initialState = { firstName: "",lastName: "", email: "", password: "" }
+const initialState = { firstName: "", lastName: "", email: "", password: "" }
 export default function Login() {
   const { dispatch } = useAuthContext()
   const [messageApi, contextHolder] = message.useMessage();
@@ -27,7 +27,7 @@ export default function Login() {
   const handleLogin = e => {
     e.preventDefault()
     const { firstName, lastName, email, password } = state
-    
+
     if (firstName.length < 3) {
       return (
         massegeAlert('warning', 'Plz! Enter fullname')
@@ -59,29 +59,33 @@ export default function Login() {
   return (
     <>
       {contextHolder}
-      <div className="container my-5">
+      <div className="container login ">
         <div className="row">
-          <div className="col-12 col-md-6 col-md-6 m-auto ">
-            <div className="card p-3 ">
+          <div className="col">
+            <div className="card p-3">
               <h1 className='text-center'>Login</h1>
-              <form className="row g-3">
-                <div className="col-md-6">
-                  <label htmlFor="firstname" className="form-label">Firstname</label>
-                  <input type="text" className="form-control" id='firstname' name='firstName' value={state.firstName} onChange={handlechange} />
+              <form >
+                <div className="row ">
+                  <div className="col-md-6 col-lg-6">
+                    <label htmlFor="firstname" className="form-label">Firstname</label>
+                    <input type="text" className="form-control" id='firstname' name='firstName' value={state.firstName} onChange={handlechange} />
+                  </div>
+                  <div className="col-md-6 col-lg-6">
+                    <label htmlFor="lastname" className="form-label">Lastname</label>
+                    <input type="text" className="form-control" id='lastname' name='lastName' value={state.lastName} onChange={handlechange} />
+                  </div>
                 </div>
-                <div className="col-md-6">
-                  <label htmlFor="lastname" className="form-label">Lastname</label>
-                  <input type="text" className="form-control" id='lastname' name='lastName' value={state.lastName} onChange={handlechange} />
+                <div className="row ">
+                  <div className="col-md-6 col-lg-6">
+                    <label htmlFor="inputEmail" className="form-label">Email</label>
+                    <input type="email" className="form-control" id='inputEmail' name='email' value={state.email} onChange={handlechange} />
+                  </div>
+                  <div className="col-md-6 col-lg-6">
+                    <label htmlFor="inputPassword" className="form-label">Password</label>
+                    <input type="password" className="form-control" id='inputPassword' name='password' value={state.password} onChange={handlechange} />
+                  </div>
                 </div>
-                <div className="col-md-6">
-                  <label htmlFor="inputEmail" className="form-label">Email</label>
-                  <input type="email" className="form-control" id='inputEmail' name='email' value={state.email} onChange={handlechange} />
-                </div>
-                <div className="col-md-6">
-                  <label htmlFor="inputPassword" className="form-label">Password</label>
-                  <input type="password" className="form-control" id='inputPassword' name='password' value={state.password} onChange={handlechange} />
-                </div>
-                <div className="col-12 text-center">
+                <div className="col-12 text-center mt-3">
                   <button type="submit" className="btn btn-primary w-50" onClick={handleLogin}>Sign in</button>
                 </div>
               </form>
